fb004e127589a7d139907037e28f1a8824a46ce4
travisredd <travisredd@mail.weber.edu>
2019-07-02 23:15:31 -0600

    ....

--- PiePizzeria4/Models/ViewModels/AddEditPizzaModel.cs
+++ PiePizzeria4/Models/ViewModels/AddEditPizzaModel.cs
@@ -18,20 +18,6 @@ namespace PiePizzeria.ViewModels
 
         public string Title { get; set; }
 
-        //public int Id { get; set; }
-        //[Required(ErrorMessage = "The Name must be specified")]
-        //[StringLength(25, ErrorMessage = "Name must be between 5 and 25 characters", MinimumLength = 5)]
-        //public string Name { get; set; }
-        //[StringLength(50, ErrorMessage = "Short Description cannot be more than 50 characters")]
-        //public string ShortDescription { get; set; }
-        //[StringLength(1000, ErrorMessage = "Long Description cannot be more than 1000 characters")]
-        //public string LongDescription { get; set; }
-        //[Required(ErrorMessage = "The Price must be specified")]
-        //public decimal Price { get; set; }
-        ////public string ImageUrl { get; set; }
-        ////public string ImageThumbnailUrl { get; set; }
-        //public bool IsPieOfTheWeek { get; set; }
-
         public AddEditPizzaModel()
         {
 
